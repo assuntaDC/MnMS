ec75bccd5eb12c2cbffdadf0b64f03bec6d44b37
adecaro <decaroassunta98@gmail.com>
2024-12-16 08:48:59 +0100

    time, main, decision model introduction

---
+++ examples/public_transport/desperation.py
@@ -0,0 +1,141 @@
+import os
+import time
+
+import numpy as np
+
+from mnms.simulation import Supervisor
+from mnms.demand import CSVDemandManager
+from mnms.flow.MFD import Reservoir, MFDFlowMotor
+from mnms.log import attach_log_file, LOGLEVEL, set_mnms_logger_level
+from mnms.time import Time, Dt
+from mnms.io.graph import load_graph, load_odlayer, save_graph, save_odlayer, save_transit_link_odlayer, \
+    load_transit_links
+from mnms.travel_decision.behavior_and_congestion_decision_model import BehaviorCongestionDecisionModel
+from mnms.travel_decision.logit import LogitDecisionModel
+from mnms.tools.observer import CSVUserObserver, CSVVehicleObserver
+from mnms.generation.layers import generate_bbox_origin_destination_layer
+from mnms.mobility_service.personal_vehicle import PersonalMobilityService
+from mnms.mobility_service.public_transport import PublicTransportMobilityService
+import json
+
+import pandas as pd
+
+indir = "INPUTS"
+outdir = "OUTPUTS"
+
+# set_all_mnms_logger_level(LOGLEVEL.WARNING)
+set_mnms_logger_level(LOGLEVEL.INFO, ["mnms.simulation"])
+
+#get_logger("mnms.graph.shortest_path").setLevel(LOGLEVEL.WARNING)
+attach_log_file(outdir + '/simulation.log')
+
+
+# 'DESTINATION_R_82604106' 'ORIGIN_E_83202447'
+
+def calculate_V_MFD(acc):
+    #V = 10.3*(1-N/57000) # data from fit prop
+    V = 0  # data from fit dsty
+    N = acc["CAR"]
+    if N < 18000:
+        V = 11.5 - N * 6 / 18000
+    elif N < 55000:
+        V = 11.5 - 6 - (N - 18000) * 4.5 / (55000 - 18000)
+    elif N < 80000:
+        V = 11.5 - 6 - 4.5 - (N - 55000) * 1 / (80000 - 55000)
+    #V = 11.5*(1-N/60000)
+    V = max(V, 0.001)  # min speed to avoid gridlock
+    V_TRAM_BUS = 0.7 * V
+    return {"CAR": V, "METRO": 17, "BUS": V_TRAM_BUS, "TRAM": V_TRAM_BUS}
+
+
+def load_capacity_info(capacity_file):
+    print("Loading capacity info from {}".format(capacity_file))
+    capacity_info = {}
+    with open(capacity_file, 'r') as f:
+        pt_network = json.load(f)
+        for i in range(len(pt_network['LAYERS'])):
+            layer = pt_network['LAYERS'][i]
+            layer_id = layer['ID']
+            if 'METRO' in layer_id or 'BUS' in layer_id or 'TRAM' in layer_id:
+                for k in range(len(layer['LINES'])):
+                    veh = layer['LINES'][k]['ID']
+                    capacity_info[veh] = layer['LINES'][k]['CAPACITY']
+    return capacity_info
+
+
+def force_public_transport(demand_file):
+    print('Forcing public transport from {}'.format(demand_file))
+    demand_data = pd.read_csv(demand_file, sep=';')
+    print('N queries:', len(demand_data), 'N users:', len(np.unique(demand_data['ID'])))
+    if 'MOBILITY SERVICES' not in demand_data.columns:
+        demand_data['MOBILITY SERVICES'] = 'METRO TRAM BUS'
+        demand_data.to_csv(demand_file, sep=';', index=False)
+
+
+if __name__ == '__main__':
+    NX = 100
+    NY = 100
+    #DIST_CONNECTION = 1e2
+
+    mmgraph = load_graph(indir + "/lyon_network_gtfs_mod.json")
+    start_time = time.time()
+    odlayer = generate_bbox_origin_destination_layer(mmgraph.roads, NX, NY)
+    mmgraph.odlayer = odlayer
+    end_time = time.time()
+    print('OD LAYER CREATION', end_time - start_time, 's')
+
+    ##
+    # start_time = time.time()
+    mmgraph.add_origin_destination_layer(odlayer)
+    # mmgraph.connect_origindestination_layers(500, 1000)
+    # end_time = time.time()
+    # print('MMGRAPH LAYER CREATION', end_time - start_time, 's')
+
+    if not os.path.exists(indir + f"/transit_link_{NX}_{NY}_{500}_grid.json"):
+        mmgraph.connect_origindestination_layers(500,1000)
+        save_transit_link_odlayer(mmgraph, indir + f"/transit_link_{NX}_{NY}_{500}_grid.json")
+    else:
+        start_time = time.time()
+        load_transit_links(mmgraph, indir + f"/transit_link_{NX}_{NY}_{500}_grid.json")
+        end_time = time.time()
+        print('MMGRAPH LAYER UPLOADING', end_time - start_time, 's')
+
+    personal_car = PersonalMobilityService()
+    personal_car.attach_vehicle_observer(CSVVehicleObserver(outdir + "/veh.csv"))
+    mmgraph.layers["CAR"].add_mobility_service(personal_car)
+
+    capacity_info = load_capacity_info(indir + "/lyon_network_gtfs_mod.json")
+
+    bus_service = PublicTransportMobilityService("BUS", capacity_info=capacity_info)
+    bus_service.attach_vehicle_observer(CSVVehicleObserver(outdir + "/veh.csv"))
+    mmgraph.layers["BUSLayer"].add_mobility_service(bus_service)
+
+    tram_service = PublicTransportMobilityService("TRAM", capacity_info=capacity_info)
+    tram_service.attach_vehicle_observer(CSVVehicleObserver(outdir + "/veh.csv"))
+    mmgraph.layers["TRAMLayer"].add_mobility_service(tram_service)
+
+    metro_service = PublicTransportMobilityService("METRO", capacity_info=capacity_info)
+    metro_service.attach_vehicle_observer(CSVVehicleObserver(outdir + "/veh.csv"))
+    mmgraph.layers["METROLayer"].add_mobility_service(metro_service)
+
+    demand_file_name = indir + "/demand_custom.csv"
+    force_public_transport(demand_file_name)
+    demand = CSVDemandManager(demand_file_name)
+    demand.add_user_observer(CSVUserObserver(outdir + "/user.csv"), user_ids="all")
+
+    flow_motor = MFDFlowMotor(outfile=outdir + "/flow.csv")
+    flow_motor.add_reservoir(Reservoir(mmgraph.roads.zones["RES"], ["CAR"], calculate_V_MFD))
+
+    #travel_decision = LogitDecisionModel(mmgraph, outfile=outdir + "/path.csv")
+    travel_decision = BehaviorCongestionDecisionModel(mmgraph, outfile=outdir + "/path.csv", alpha=0, beta=0)
+
+    supervisor = Supervisor(graph=mmgraph,
+                            flow_motor=flow_motor,
+                            demand=demand,
+                            decision_model=travel_decision,
+                            outfile=outdir + "/travel_time_link.csv")
+
+    start = time.time()
+    supervisor.run(Time('16:41:00'), Time('20:30:00'), Dt(seconds=30), 10)
+    end = time.time()
+    print(f'SIMULATION COMPLETED IN {end-start} s')
---
+++ src/mnms/travel_decision/behavior_and_congestion_decision_model.py
@@ -0,0 +1,118 @@
+import logging
+from math import exp, fsum
+from typing import List, Tuple
+
+import numpy as np
+
+from mnms import create_logger
+from mnms.demand.user import Path
+from mnms.time import Time
+from mnms.travel_decision.abstract import AbstractDecisionModel
+from mnms.graph.layers import MultiLayerGraph
+import pandas as pd
+
+log = create_logger(__name__)
+
+
+class BehaviorCongestionDecisionModel(AbstractDecisionModel):
+    def __init__(self, mmgraph: MultiLayerGraph, considered_modes=None, cost='travel_time', outfile: str = None,
+                 verbose_file=False, alpha=1, beta=1, gamma=1):
+        """Behavior- and congestion-driven decision model for the path of a user.
+        All routes computed are considered on an equal footing for the choice.
+
+        Args:
+            -mmgraph: The graph on which the model compute the path
+            -considered_modes: List of guidelines for the guided paths discovery,
+                           if None, the default paths discovery is applied
+            -cost: name of the cost to consider
+            -outfile: Path to result CSV file, nothing is written if None
+            -verbose_file: If True write all the computed shortest path, not only the one that is selected
+            -personal_mob_service_park_radius: radius around user's personal veh parking location in which
+                                               she can still have access to her vehicle
+            -save_routes_dynamically_and_reapply: boolean specifying if the k shortest paths computed
+                                                  for an origin, destination, and mode should be saved
+                                                  dynamically and reapply for next departing users with
+                                                  the same origin, destination and mode
+        """
+        super(BehaviorCongestionDecisionModel, self).__init__(mmgraph,
+                                                              considered_modes=considered_modes,
+                                                              cost=cost,
+                                                              outfile=outfile,
+                                                              verbose_file=verbose_file,
+                                                              #alpha=alpha,
+                                                              #beta=beta,
+                                                              #gamma=gamma
+                                                              )
+        self._seed = None
+        self._rng = None
+        self.alpha = alpha
+        self.beta = beta
+        self.gamma = gamma
+        assert cost == 'travel_time'
+        if self.alpha != 0:
+            self.CI_data = pd.read_csv('OUTPUTS/congestion_file_backup.csv')
+            self.CI_data.TIMESTAMP = pd.to_datetime(self.CI_data.TIMESTAMP, format='mixed')
+
+    def set_random_seed(self, seed):
+        """Method that sets the random seed for this decision model.
+
+        Args:
+            -seed: seed as an integer
+        """
+        if seed is not None:
+            self._seed = seed
+            rng = np.random.default_rng(self._seed)
+            self._rng = rng
+
+    def path_choice(self, paths: List[Path], tcurrent) -> Path:
+        """Method that proceeds to the selection of the path.
+
+        Args:
+            -paths: list of paths to consider for the choice
+
+        Returns:
+            -selected_path: path chosen
+        """
+        path_score = []
+
+        # EXTRACT THE LONGEST PATH AMONG ALL PATHS
+        max_path_cost = 0
+        for path in paths:
+            path_cost = path.path_cost
+            if path_cost > max_path_cost:
+                max_path_cost = path_cost
+                
+
+        # base cost
+        for path in paths:
+            score = 0
+            path_tt = path.get_link_cost(self._mlgraph, self._cost)
+            # EXCLUDE THE ORIGIN AND DESTINAION FROM COMPUTATION
+            i = 0
+            if self.alpha != 0 and self.beta != 0:
+                line = path.nodes[1].split('_')[0]
+                for x in path.nodes[1:-1]:
+                    next_line = x.split('_')[0] + x.split('_')[1]
+                    if line != next_line or i == 0:
+                        t = sum(path_tt[:i]) + tcurrent
+                        line = next_line
+                        score += self.alpha * (1 - self.get_CI(x, t)) + self.beta * self.get_BI(x, t)
+                    # TO CHECK
+                    i += 1
+            C = path.path_cost / max_path_cost
+            score += self.gamma * (1 - C)
+            path_score.append(score)
+
+        return paths[np.argmax(path_score)] if len(path_score) > 0 else None
+
+    def get_CI(self, node, tcurrent):
+        CI = self.CI_data[self.CI_data['NODE'] == node].copy(deep=True)
+        tcurrent_datetime = pd.to_datetime(str(tcurrent))
+        CI['time_diff'] = [(x - tcurrent_datetime).total_seconds() for x in CI.TIMESTAMP]
+        CI = CI[CI['time_diff'] >= 0]
+        CI = CI.sort_values(by=['time_diff', 'CONGESTION INDEX']).reset_index(drop=True, inplace=False)
+        print('node', node, tcurrent, CI.loc[0, 'CONGESTION INDEX'])
+        return CI['CONGESTION INDEX'][0]
+
+    def get_BI(self, x, tcurrent):
+        return 1
